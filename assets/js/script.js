943675ae51f23a67c2e80fdb3290c772ac184638
Tintin <tanner@schenck.xyz>
2020-09-21 23:40:59 -0400

    Current weather is now available to start putting on the page dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,34 @@
 $(document).ready(function () {
   // console.log("ready!!!");
 
-
-    $("#weather-btn").on("click", function (event) {
+  $("#weather-btn").on("click", function (event) {
     event.preventDefault();
 
     var cityInput = $("#weather-input").val().trim();
     console.log(cityInput);
-    currentWeather()
+    currentWeather(cityInput);
+  });
 
-    })
-  function currentWeather() {
+  function currentWeather(city) {
     $.ajax({
       method: "GET",
       url:
-        "https://api.openweathermap.org/data/2.5/weather?q=tampa&appid=96c11cefb1ce135a21376c8104974b72",
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=96c11cefb1ce135a21376c8104974b72&units=imperial",
     }).then(function (data) {
       console.log(data);
-   
-   
-   
-    })
+    });
   }
+    function forecast() {
+      $.ajax({
+        method: "GET",
+        url:
+          "https://api.openweathermap.org/data/2.5/forecast?q=" +
+          city +
+          "&appid=96c11cefb1ce135a21376c8104974b72&units=imperial",
+      }).then(function (data) {
+        console.log(data);
+      });
+    }
 });
-
